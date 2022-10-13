4bd1e0914e6189bbb82d2b2defd9f39f793acde8
楊志庄 <21cr0141@jec.ac.jp>
2022-10-13 23:32:53 +0900

    フェーズAからフェーズBに遷移する処理をフェーズマネージャーに統合

--- Assets/Scripts/Phase/PhaseManager.cs
+++ Assets/Scripts/Phase/PhaseManager.cs
@@ -10,18 +10,30 @@ public class PhaseManager : MonoBehaviour
     [SerializeField] private float phaseBTime = 20f;
     [SerializeField] private float phaseCTime = 20f;
 
+    private GameObject _player;
+
     private void Start()
     {
+        _player = FindObjectOfType<PhaseA_TargetLook>().gameObject;
+        
         _stateMachine.AddNode(GamePhase.A, () => { }, () =>
         {
             Debug.Log("Enter phase A");
             Timeline.RestartTimer();
-        }, () => { Debug.Log("Leave phase A"); });
+        }, () =>
+        {
+            Debug.Log("Leave phase A"); 
+            FindObjectOfType<Doragon_Move_A_Test>().enabled = false;
+            FindObjectOfType<Doragon_Attack_A_Test>().enabled = false;
+            FindObjectOfType<PhaseA_TargetLook>().enabled = false;
+        });
 
         _stateMachine.AddNode(GamePhase.B, () => { }, () =>
         {
             Debug.Log("Enter phase B");
             Timeline.ResetTimer();
+            _player.GetComponent<PlayerMove_1005>().enabled = true;
+            _player.GetComponent<PhaseA_PhaseB>().enabled = true;
         }, () => { Debug.Log("Leave phase B"); });
 
         _stateMachine.AddNode(GamePhase.C, () => { }, () =>
