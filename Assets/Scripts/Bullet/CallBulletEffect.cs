a854af818733da148025126541c38d839198be18
DESKTOP-M8IHVJQ\erik0 <erik0106@icloud.com>
2022-10-14 01:20:00 +0900

    報告用動画で使うエフェクト入れ 弾から炎が噴き出すようにしました。 エフェクトが消えるようにしました

--- Assets/Scripts/Bullet/CallBulletEffect.cs
+++ Assets/Scripts/Bullet/CallBulletEffect.cs
@@ -8,17 +8,17 @@ public class CallBulletEffect : MonoBehaviour
     public GameObject fire;
     void Start()
     {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         var pos = this.gameObject.transform.position;
             
         var t = Instantiate(fire) as GameObject;
         t.transform.position = pos;
             
         Vector3 vec = bullet.transform.position - pos;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
