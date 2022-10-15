baaad0466a5a8e58d4d81c94f1acc28e732fa257
楊志庄 <21cr0141@jec.ac.jp>
2022-10-15 14:10:06 +0900

    Player Move

--- Assets/Scripts/Player/PhaseA_PhaseB.cs
+++ Assets/Scripts/Player/PhaseA_PhaseB.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PhaseA_PhaseB : MonoBehaviour
 {
+    [SerializeField] private Vector3 phaseBPos= new Vector3(495,3.26f,50f);
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(495,3.26f,6.42f);
-        transform.LookAt(new Vector3(495f,3.26f,10f));
+        transform.position = phaseBPos;
+        transform.LookAt(phaseBPos+Vector3.forward);
     }
 
     // Update is called once per frame
