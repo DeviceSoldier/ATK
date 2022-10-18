40a4452c0ac98226c69f0f4d039ee8f17d360c41
楊志庄 <21cr0141@jec.ac.jp>
2022-10-18 10:36:35 +0900

    player gage

--- Assets/Scripts/Gage/Gage.cs
+++ Assets/Scripts/Gage/Gage.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-public class Gage : MonoBehaviour
+public class Gage : IGage<float>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private float _value = 0f;
+	private float _maxValue = 100f;
+	private float _minValue = 0f;
+	private float _duration = 100f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float Value => _value;
+
+	public float MaxValue => _maxValue;
+
+	public float MinValue => _minValue;
+
+	public float Percentage => (_value - _minValue) / _duration;
+
+	public float Add(float value)
+	{
+		_value += value;
+
+		if (_value > _maxValue)
+			_value = _maxValue;
+		else if (_value < _minValue)
+			_value = _minValue;
+
+		return _value;
+	}
+
+	public void SetMax(float value)
+	{
+		_maxValue = value;
+		_duration = _maxValue - _minValue;
+		Assert.IsTrue(_duration > 0);
+	}
+
+	public void SetMin(float value)
+	{
+		_minValue = value;
+		_duration = _maxValue - _minValue;
+		Assert.IsTrue(_duration > 0);
+	}
+
+	public void SetValue(float value)
+	{
+		_value = value;
+
+		if (_value > _maxValue)
+			_value = _maxValue;
+		else if (_value < _minValue)
+			_value = _minValue;
+	}
 }
