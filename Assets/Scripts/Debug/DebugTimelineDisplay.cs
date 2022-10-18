34de9672e4d7f63ba56c1c90e6b36cec27eec4ce
楊志庄 <21cr0141@jec.ac.jp>
2022-10-18 13:49:09 +0900

    debug display phase

--- Assets/Scripts/Debug/DebugTimelineDisplay.cs
+++ Assets/Scripts/Debug/DebugTimelineDisplay.cs
@@ -6,18 +6,20 @@ using UnityEngine;
 #if UNITY_EDITOR
 public class DebugTimelineDisplay : MonoBehaviour
 {
-    private GUIStyle style = new GUIStyle();
-    private Rect rect = new Rect(20, 20, 100, 100);
+	private GUIStyle style = new GUIStyle();
+	private Rect rect = new Rect(20, 20, 100, 100);
+	private PhaseManager phaseManager;
 
-    private void Start()
-    {
-        style.fontSize = 32;
-        style.normal.textColor = Color.red;
-    }
+	private void Start()
+	{
+		style.fontSize = 32;
+		style.normal.textColor = Color.red;
+		phaseManager = FindObjectOfType<PhaseManager>();
+	}
 
-    private void OnGUI()
-    {
-        GUI.Label(rect, ((int)Timeline.CurrentTime).ToString(), style);
-    }
+	private void OnGUI()
+	{
+		GUI.Label(rect, $"{phaseManager.StateMachine.CurrentNodeId}:{((int)Timeline.CurrentTime).ToString()}", style);
+	}
 }
 #endif
--- Assets/Scripts/Phase/StateMachine/StateMachine.cs
+++ Assets/Scripts/Phase/StateMachine/StateMachine.cs
@@ -5,81 +5,83 @@ using UnityEngine;
 
 public class StateMachine<T>
 {
-    private Dictionary<T, StateMachineNode<T>> _nodes;
-    private T _currentNodeId;
+	private Dictionary<T, StateMachineNode<T>> _nodes;
+	private T _currentNodeId;
+	public T CurrentNodeId => _currentNodeId;
+	public StateMachineNode<T> CurrentNode => _nodes[_currentNodeId];
 
-    public StateMachine()
-    {
-        _nodes = new Dictionary<T, StateMachineNode<T>>();
-    }
+	public StateMachine()
+	{
+		_nodes = new Dictionary<T, StateMachineNode<T>>();
+	}
 
-    public void SetEntry(T nodeId)
-    {
-        if (!_nodes.ContainsKey(nodeId))
-            return;
-        
-        _currentNodeId = nodeId;
-        _nodes[nodeId].Enter();
-    }
+	public void SetEntry(T nodeId)
+	{
+		if (!_nodes.ContainsKey(nodeId))
+			return;
 
-    public void AddNode(StateMachineNode<T> node)
-    {
-        var key = node.NodeId;
-        if (_nodes.ContainsKey(key))
-            _nodes[key] += node;
-        else
-            _nodes.Add(key, node);
-    }
+		_currentNodeId = nodeId;
+		_nodes[nodeId].Enter();
+	}
 
-    public void AddNode(T nodeId, Action onUpdate, Action onEnter, Action onLeave)
-    {
-        StateMachineNode<T> node = new StateMachineNode<T>(nodeId);
-        node.AddUpdateFunc(onUpdate);
-        node.AddEnterFunc(onEnter);
-        node.AddLeaveFunc(onLeave);
-        AddNode(node);
-    }
+	public void AddNode(StateMachineNode<T> node)
+	{
+		var key = node.NodeId;
+		if (_nodes.ContainsKey(key))
+			_nodes[key] += node;
+		else
+			_nodes.Add(key, node);
+	}
 
-    public void AddEdge(T nodeId, StateMachineEdge<T> edge)
-    {
-        if (!_nodes.ContainsKey(nodeId))
-            return;
-        _nodes[nodeId].AddEdge(edge);
-    }
+	public void AddNode(T nodeId, Action onUpdate, Action onEnter, Action onLeave)
+	{
+		StateMachineNode<T> node = new StateMachineNode<T>(nodeId);
+		node.AddUpdateFunc(onUpdate);
+		node.AddEnterFunc(onEnter);
+		node.AddLeaveFunc(onLeave);
+		AddNode(node);
+	}
 
-    public void AddEdge(T from, T to, Func<bool> condition)
-    {
-        StateMachineEdge<T> edge = new StateMachineEdge<T>(to);
-        edge.AddTransformConditionFunc(condition);
-        AddEdge(from, edge);
-    }
+	public void AddEdge(T nodeId, StateMachineEdge<T> edge)
+	{
+		if (!_nodes.ContainsKey(nodeId))
+			return;
+		_nodes[nodeId].AddEdge(edge);
+	}
 
-    private void SetState(T nodeId)
-    {
-        if (!_nodes.ContainsKey(nodeId))
-            return;
-        _nodes[_currentNodeId].Leave();
-        _currentNodeId = nodeId;
-        _nodes[nodeId].Enter();
-    }
+	public void AddEdge(T from, T to, Func<bool> condition)
+	{
+		StateMachineEdge<T> edge = new StateMachineEdge<T>(to);
+		edge.AddTransformConditionFunc(condition);
+		AddEdge(from, edge);
+	}
 
-    public void Update()
-    {
-        if (!_nodes.ContainsKey(_currentNodeId))
-            return;
+	private void SetState(T nodeId)
+	{
+		if (!_nodes.ContainsKey(nodeId))
+			return;
+		_nodes[_currentNodeId].Leave();
+		_currentNodeId = nodeId;
+		_nodes[nodeId].Enter();
+	}
 
-        var transformed = false;
-        var currentNode = _nodes[_currentNodeId];
-        var edges = currentNode.Edges;
-        foreach (var edge in edges.Values)
-            if (edge.CanTransform)
-            {
-                SetState(edge.NextNodeId);
-                transformed = true;
-                break;
-            }
+	public void Update()
+	{
+		if (!_nodes.ContainsKey(_currentNodeId))
+			return;
 
-        if (!transformed)
-            currentNode.Update();
-    }
+		var transformed = false;
+		var currentNode = _nodes[_currentNodeId];
+		var edges = currentNode.Edges;
+		foreach (var edge in edges.Values)
+			if (edge.CanTransform)
+			{
+				SetState(edge.NextNodeId);
+				transformed = true;
+				break;
+			}
+
+		if (!transformed)
+			currentNode.Update();
+	}
 }
