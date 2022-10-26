63fff14c747a9d29e1d37b82f6d1a4abfe56eab6
楊志庄 <21cr0141@jec.ac.jp>
2022-10-27 01:16:27 +0900

    dragon hp gage

--- Assets/Scripts/Enemys/Boss/BossHP.cs
+++ Assets/Scripts/Enemys/Boss/BossHP.cs
@@ -1,12 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHP : HPObject
 {
     public GameObject deleteobj;
+    public Slider Slider;
+    
     protected override void OnHpZero()
     {
         Destroy(deleteobj);
+    }
+
+    private void Start()
+    {
+        Slider.maxValue = MaxHp;
+    }
+
+    private void Update()
+    {
+        Slider.value = Hp;
     }
 }
